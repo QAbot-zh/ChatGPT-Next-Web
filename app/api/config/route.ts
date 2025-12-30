27dd48b2af72850c0891ea1e31b9c2d007263cfa
undefinedcodezhong <40236765+undefinedcodezhong@users.noreply.github.com>
2025-12-30 20:52:02 +0800

    fix: 简化 api/config 修复，只添加 dynamic force-static

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -5,44 +5,42 @@ import { getServerSideConfig, getSidebarConfig } from "../../config/server";
 // Required for static export
 export const dynamic = "force-static";
 
+const serverConfig = getServerSideConfig();
+const siderbarConfig = getSidebarConfig();
+
 // Danger! Do not hard code any secret value here!
 // 警告！不要在这里写入任何敏感信息！
-function getDangerConfig() {
-  const serverConfig = getServerSideConfig();
-  const siderbarConfig = getSidebarConfig();
-
-  return {
-    needCode: serverConfig.needCode,
-    hideUserApiKey: serverConfig.hideUserApiKey,
-    disableGPT4: serverConfig.disableGPT4,
-    hideBalanceQuery: serverConfig.hideBalanceQuery,
-    disableFastLink: serverConfig.disableFastLink,
-    customModels: serverConfig.customModels,
-    defaultModel: serverConfig.defaultModel,
-    visionModels: serverConfig.visionModels,
-    compressModel: serverConfig.compressModel,
-    customHello: serverConfig.customHello,
-    UnauthorizedInfo: serverConfig.UnauthorizedInfo,
-    iconPosition: serverConfig.iconPosition,
-    // translateModel: serverConfig.translateModel,
-    textProcessModel: serverConfig.textProcessModel,
-    ocrModel: serverConfig.ocrModel,
-    sidebarTitle: siderbarConfig.title,
-    sidebarSubTitle: siderbarConfig.subTitle,
-    siteTitle: siderbarConfig.siteTitle,
-    selectLabels: serverConfig.selectLabels,
-    modelParams: serverConfig.modelParams,
-    // imageBed config
-    imgUploadApiUrl: serverConfig.imgUploadApiUrl,
-  };
-}
+const DANGER_CONFIG = {
+  needCode: serverConfig.needCode,
+  hideUserApiKey: serverConfig.hideUserApiKey,
+  disableGPT4: serverConfig.disableGPT4,
+  hideBalanceQuery: serverConfig.hideBalanceQuery,
+  disableFastLink: serverConfig.disableFastLink,
+  customModels: serverConfig.customModels,
+  defaultModel: serverConfig.defaultModel,
+  visionModels: serverConfig.visionModels,
+  compressModel: serverConfig.compressModel,
+  customHello: serverConfig.customHello,
+  UnauthorizedInfo: serverConfig.UnauthorizedInfo,
+  iconPosition: serverConfig.iconPosition,
+  // translateModel: serverConfig.translateModel,
+  textProcessModel: serverConfig.textProcessModel,
+  ocrModel: serverConfig.ocrModel,
+  sidebarTitle: siderbarConfig.title,
+  sidebarSubTitle: siderbarConfig.subTitle,
+  siteTitle: siderbarConfig.siteTitle,
+  selectLabels: serverConfig.selectLabels,
+  modelParams: serverConfig.modelParams,
+  // imageBed config
+  imgUploadApiUrl: serverConfig.imgUploadApiUrl,
+};
 
 declare global {
-  type DangerConfig = ReturnType<typeof getDangerConfig>;
+  type DangerConfig = typeof DANGER_CONFIG;
 }
 
 async function handle() {
-  return NextResponse.json(getDangerConfig());
+  return NextResponse.json(DANGER_CONFIG);
 }
 
 export const GET = handle;
